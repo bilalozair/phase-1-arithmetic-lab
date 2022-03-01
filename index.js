39cb313005b2cc57661acb89b7a0071ead00e172
bilalozair <bo569@nyu.edu>
2022-03-01 15:17:11 +0500

    commit tree 31b3b2d963591a87a4cef9098456574de7e65d70

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
-
 // Write your code here
+let num1 = 2;
+let num2 = 31;
+let num3 = 24;
+let num4 = 5;
+let multiply = num1*num2;
+
+let random = Math.ceil(Math.random()+1)
+let mod = num3 % num4;
+let max = Math.max(1,2,3,20);
